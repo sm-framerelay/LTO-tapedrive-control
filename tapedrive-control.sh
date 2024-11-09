7e99a8577e0cbc66b6703a75bdd69117ef2d3dba
framerelay <melnik.s.p@gmail.com>
2024-11-09 23:49:10 +0300

    Spaces and tabs alligment in utility sources

--- tapedrive-control.sh
+++ tapedrive-control.sh
@@ -49,25 +49,25 @@ NC='\033[0m'
 
 # Function to print a message in a specified color, defaulting to NC if the color is not defined
 function print_message() {
-    local color="$1"
-    local message="$2"
+	local color="$1"
+	local message="$2"
 
-    # Check if color is empty or undefined, default to NC
-    if [ -z "$color" ]; then
-        color=$NC
-    fi
+	# Check if color is empty or undefined, default to NC
+	if [ -z "$color" ]; then
+		color=$NC
+	fi
 
-    echo -e "${color}${message}${NC}"
+	echo -e "${color}${message}${NC}"
 }
 
 # Function to print an error message
 function print_error() {
-    print_message "$RED" "${1}"
+	print_message "$RED" "${1}"
 }
 
 # Function to print a warning message
 function print_warn() {
-    print_message "$YELLOW" "${1}"
+	print_message "$YELLOW" "${1}"
 }
 # Function which interrupts flow and ask used to press any key
 function press_any_continue() {
@@ -102,22 +102,22 @@ function write_warning_and_confirm() {
 	echo "If the data will be written to the wrong position, it might damage or"
 	echo "overwrite the existing data on the tape. Please ensure that you"
 	echo -e "carefully select the correct actions before proceeding.\n"
-    echo -en "${BRIGHT_WHITE}Are you sure you want to continue? (yes/no)${NC}: "
-    read confirm
+	echo -en "${BRIGHT_WHITE}Are you sure you want to continue? (yes/no)${NC}: "
+	read confirm
 	
-    case $confirm in
-        [Yy][Ee][Ss])
-            return 0  # Success status
-            ;;
-        [Nn][Oo])
-            echo "Returning to the main menu."
-            show_menu
-            ;;
-        *)
-            echo "Invalid input, please enter yes or no."
-            warning_and_confirm
-            ;;
-    esac
+	case $confirm in
+		[Yy][Ee][Ss])
+			return 0  # Success status
+			;;
+		[Nn][Oo])
+			echo "Returning to the main menu."
+			show_menu
+		 	;;
+		*)
+			echo "Invalid input, please enter yes or no."
+			warning_and_confirm
+			;;
+	esac
 }
 
 # Function which shows warning that all content from the directory will be written on tape
@@ -126,21 +126,21 @@ function are_you_sure_to_write_all(){
 	echo "all the data from the current directory to the tape."
 	echo "Depends on tape position this action will overwrite an existing data on the tape,"
 	echo -e "making any previously stored information irretrievable.\n"
-    echo -en "${BRIGHT_WHITE}Are you sure to proceed with this operation? (yes/no)${NC}:"
+	echo -en "${BRIGHT_WHITE}Are you sure to proceed with this operation? (yes/no)${NC}:"
 	
 	read confirm
 	
-    case $confirm in
-        [Yy][Ee][Ss])
-            return 0  # Success status
-            ;;
-        [Nn][Oo])
-            return 1
-            ;;
-        *)
-            return -1
-            ;;
-    esac
+	case $confirm in
+		[Yy][Ee][Ss])
+			return 0  # Success status
+			;;
+		[Nn][Oo])
+			return 1
+			;;
+		*)
+			return -1
+			;;
+	esac
 }
 
 # Function which checks mt exist status and terminates flow if there is an error
@@ -168,8 +168,8 @@ function check_terminate_existstatus(){
 
 # Function to display binary header
 function show_binary_header {
-    print_message "$BRIGHT_CYAN" "> Binary header of the current record:"
-    dd if=$TAPE_DEVICE bs="${DD_BLOCK_SIZE}K" count=1 2>/dev/null | hexdump -C | head -n 10
+	print_message "$BRIGHT_CYAN" "> Binary header of the current record:"
+	dd if=$TAPE_DEVICE bs="${DD_BLOCK_SIZE}K" count=1 2>/dev/null | hexdump -C | head -n 10
 }
 
 # Function which checks values validity and set tape block size variable 
@@ -201,21 +201,21 @@ function read_or_list_content(){
 	echo -e "Are you going to just list the tape's content, or read and save its content to the current working directory?\n"
 	echo -e "*\tType [${BRIGHT_WHITE}read${NC}] if you are going to restore content from the tape to disk"
 	echo -e "*\tPress [${BRIGHT_WHITE}ENTER${NC}] if you like just to list the conent\n"
-    echo -en "${BRIGHT_WHITE}Press ENTER or type 'read' to continue${NC}: "
+	echo -en "${BRIGHT_WHITE}Press ENTER or type 'read' to continue${NC}: "
 	
 	read confirm
 	
 	echo ""
-    
+	
 	case $confirm in
-        [Rr][Ee][Aa][Dd])
-            echo -e "I want to [${BRIGHT_GREEN}READ${NC}] the tape and save its content\n"
+		[Rr][Ee][Aa][Dd])
+			echo -e "I want to [${BRIGHT_GREEN}READ${NC}] the tape and save its content\n"
 			read_all_content_on_tape
-            ;;
-        *)
-            echo -e "I want to [${BRIGHT_GREEN}LIST${NC}] to see what's on the tape\n"
+			;;
+		*)
+			echo -e "I want to [${BRIGHT_GREEN}LIST${NC}] to see what's on the tape\n"
 			list_all_content_on_tape
-            ;;
+			;;
     esac
 }
 
@@ -689,9 +689,9 @@ function set_tape_block_size_dialogue(){
 	echo -e "To change the value you have the following options:\n"
 	echo -e "*\tSet [${BRIGHT_WHITE}0${NC}] if you like to have variable block size"
 	echo -e "*\tSet [${BRIGHT_WHITE}64, 128, 256, 512, 1024 etc.${NC}] if you need specific block size to be set\n\n"
-    echo -en "${BRIGHT_WHITE}Enter tape block size or just press [ENTER] to kept value as is${NC}: "
+	echo -en "${BRIGHT_WHITE}Enter tape block size or just press [ENTER] to kept value as is${NC}: "
 	
-    read tapeblocksize
+	read tapeblocksize
 	
 	if [ -n "$tapeblocksize" ]; then
 		check_set_tapeblocksz "$tapeblocksize"
@@ -722,30 +722,30 @@ function set_tape_block_size_dialogue(){
 
 # Function to display the main menu
 function show_menu() {
-    clear
-    echo "|___________________________________________________________"
-    echo -e "|       ${BRIGHT_CYAN}LTO tape drive control v1.0${NC}"
-    echo "|___________________________________________________________"
-    echo "|   device:    '$TAPE_DEVICE' [block size '$TAPE_BLOCK_SIZE' KB"]
+	clear
+	echo "|___________________________________________________________"
+	echo -e "|       ${BRIGHT_CYAN}LTO tape drive control v1.0${NC}"
+	echo "|___________________________________________________________"
+	echo "|   device:    '$TAPE_DEVICE' [block size '$TAPE_BLOCK_SIZE' KB"]
 	echo "|   directory: '$WORKING_DIR'"
 	echo "|___________________________________________________________"
-    echo -e "|${BRIGHT_WHITE}MENU:${NC}"
-    echo -e "|"
-    echo -e "|  [1]. ${GREEN}<READ>${NC}  Read all content on Tape"
-    echo -e "|  [2]. ${GREEN}<READ>${NC}  Read content at exact position"
-    echo -e "|  [3]. ${RED}<WRITE>${NC} Append to the Tape new files"
-    echo -e "|  [4]. ${RED}<WRITE>${NC} Write all files to Tape"
-    echo -e "|  [5]. ${RED}<WRITE>${NC} Erase the Tape"
+	echo -e "|${BRIGHT_WHITE}MENU:${NC}"
+	echo -e "|"
+	echo -e "|  [1]. ${GREEN}<READ>${NC}  Read all content on Tape"
+	echo -e "|  [2]. ${GREEN}<READ>${NC}  Read content at exact position"
+	echo -e "|  [3]. ${RED}<WRITE>${NC} Append to the Tape new files"
+	echo -e "|  [4]. ${RED}<WRITE>${NC} Write all files to Tape"
+	echo -e "|  [5]. ${RED}<WRITE>${NC} Erase the Tape"
 	echo -e "|  [6]. ${CYAN}<UTILS>${NC} Show device status"
 	echo -e "|  [7]. ${CYAN}<UTILS>${NC} Set Tape block size"
 	echo -e "|  [8]. ${CYAN}<UTILS>${NC} Tape retension (winding to the end and back)"
 	echo -e "|  [9]. ${CYAN}<UTILS>${NC} Eject the Tape"
-    echo -e "|"
-    echo -e "|  [0]. Exit "
+	echo -e "|"
+	echo -e "|  [0]. Exit "
 	echo "|___________________________________________________________"
 	echo -e "\nNOTE:"
-    echo -e "    * ${GREEN}<READ>${NC}   Data on tape will be kept as is. Safe operations."
-    echo -e "    * ${RED}<WRITE>${NC}  Data on tape will be written at the specified position."
+	echo -e "    * ${GREEN}<READ>${NC}   Data on tape will be kept as is. Safe operations."
+	echo -e "    * ${RED}<WRITE>${NC}  Data on tape will be written at the specified position."
 	echo -en "\n\n${BRIGHT_WHITE}Enter your choice [0-9]: ${NC}"
 }
 
@@ -784,58 +784,58 @@ WORKING_DIR="$1"
 WORKING_DIR="${WORKING_DIR%/}"
 
 if [ -z "$1" ]; then
-  clear
-  print_warn "WARNING: No <working directory> provided."
-  show_usage_info
-  echo -e "${BRIGHT_GREEN}*${NC} By default if working directory not specified the current (${CYAN}'.'${NC}) directly will be used"
-  press_any_continue
-  WORKING_DIR="."
+	clear
+	print_warn "WARNING: No <working directory> provided."
+	show_usage_info
+	echo -e "${BRIGHT_GREEN}*${NC} By default if working directory not specified the current (${CYAN}'.'${NC}) directly will be used"
+	press_any_continue
+	WORKING_DIR="."
 fi
 
 
 # Check if the provided directory exists and is a directory
 if [ ! -d "$WORKING_DIR" ]; then
-  print_error "Error: The specified directory does not exist or is not a directory."
-  exit 1
+	print_error "Error: The specified directory does not exist or is not a directory."
+	exit 1
 fi
 
 shift # Shift past argument value
 
 # Parse optional arguments
 while [[ "$#" -gt 0 ]]; do
-  case "$1" in
-    -device)
-      if [ "$#" -gt 1 ]; then
-        TAPE_DEVICE="$2"
-        shift # Shift past argument value
-      else
-        print_error "Error: -device requires a device name."
-        show_usage_info
-		exit 1
-      fi
-      ;;
-    -tapeblocksz)
-      if [ "$#" -gt 1 ]; then
-        check_set_tapeblocksz "$2"
-		if [ $? -eq 1 ]; then
-			echo -e "No or invalid value provided, the Tape block is set to ${BRIGHT_GREEN}$TAPE_BLOCK_SIZE${NC} KB"
+	case "$1" in
+		-device)
+		if [ "$#" -gt 1 ]; then
+			TAPE_DEVICE="$2"
+			shift # Shift past argument value
+		else
+			print_error "Error: -device requires a device name."
 			show_usage_info
-			press_any_continue
+			exit 1
 		fi
-        shift # Shift past argument value
-      else
-        print_error "Error: -tapeblocksz requires a block size (64, 128, 256, 512, 1024, etc)"
-        show_usage_info
+		;;
+		-tapeblocksz)
+		if [ "$#" -gt 1 ]; then
+			check_set_tapeblocksz "$2"
+			if [ $? -eq 1 ]; then
+				echo -e "No or invalid value provided, the Tape block is set to ${BRIGHT_GREEN}$TAPE_BLOCK_SIZE${NC} KB"
+				show_usage_info
+				press_any_continue
+			fi
+			shift # Shift past argument value
+		else
+			print_error "Error: -tapeblocksz requires a block size (64, 128, 256, 512, 1024, etc)"
+			show_usage_info
+			exit 1
+		fi
+		;;
+		*)
+		print_error "Error: Unknown option $1."
+		show_usage_info
 		exit 1
-      fi
-      ;;
-    *)
-      print_error "Error: Unknown option $1."
-      show_usage_info
-	  exit 1
-      ;;
-  esac
-  shift # Shift past the argument key
+		;;
+	esac
+	shift # Shift past the argument key
 done
 
 main_menu_loop
